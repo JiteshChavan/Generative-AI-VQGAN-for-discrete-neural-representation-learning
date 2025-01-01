0a64250e851b84f555e2732407d4e78be34e7afe
Jitesh <jchavan010@gmail.com>
2024-12-31 19:02:17 -0500

    fused F.attention()

--- modules.py
+++ modules.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import math
 
 # TODO: try setting up a config class later to generalize the code better
 
@@ -60,35 +61,45 @@ class DownSampleBlock (nn.Module):
         X = F.pad (X, (0, 1, 0, 1), mode='constant', value=0) # Kernel size 3 makes it so that stride = 2 gives you one less than half the size so one extra row and column to fix it
         return self.conv (X)
 
-# try to incorporate pos embedding in the network itself and then multiheads within the non local block
-class NonLocalBlock (nn.Module):
-    # can refactor to send a config class in here to adjust parameters
-    def __init__ (self , channels):
+
+# TODO:  incorporate pos embedding in the network itself 
+class SelfAttention (nn.Module):
+    def __init__(self, channels, n_head):
         super().__init__()
-
-        self.in_channels = channels
+        self.channels = channels
+        self.n_head = n_head
+        assert channels % n_head == 0, f"Specified channels:{channels} are not divisible by number of attention heads{n_head}"
+        # norm
         self.group_norm = GroupNorm (channels)
-        self.conv_attn = nn.Conv2d (channels, 3 * channels, kernel_size=1, stride=1, padding=0)
-        # output projection
-        self.conv_proj = nn.Conv2d (channels, channels, kernel_size=1, stride=1, padding=0)
-
+        # attention
+        self.conv_attention = nn.Conv2d (channels, 3 * channels, kernel_size=3, stride=1, padding=1)
+        self.conv_projection = nn.Conv2d (channels, channels, kernel_size=1, stride=1, padding=0)
+    
     def forward (self, X):
         B, C, H, W = X.size()
+        # normalize X
+        x_normalized = self.group_norm (X)
+        # emit kqv
+        # X (B, C, H, W)
+        kqv = self.conv_attention (x_normalized) # (B, 3C, H, W)
+        q, k, v = kqv.split (self.channels ,dim=1) #(B,C,H,W) x3
 
-        X_normalized = self.group_norm (X)
-        qkv = self.conv_attn(X_normalized)
-        q, k, v = qkv.split (self.in_channels, dim=1) # q(B, C, H, W) k(B, C, H, W) v(B, C, H, W)
-        k = k.permute(0, 2, 3, 1).view (B, H*W, C)
-        q = q.permute(0, 2, 3, 1).view (B, H*W, C)
-        v = v.permute(0, 2, 3, 1).view (B, H*W, C)
+        # Required shape for F.scaled_dot_product_attention (B, nh, T, hs)
 
-        att = (q @ k.transpose (-2, -1)) * torch.sqrt(1.0/C) # (B, HW, HW)
-        att = F.softmax (att, dim = -1) # (B, HW, HW)
-        y = att @ v # (B, HW, HW) @ (B, HW, C) -> (B, HW, C)
+        head_size = C//self.n_head
 
-        #y = F.scaled_dot_product_attention (q, k, v, att, is_causal=False)
+        k=k.view (B, self.n_head, head_size, H*W).transpose(-2,-1) # (B, nh, HW, hs)
+        q = q.view (B, self.n_head, head_size, H*W).transpose(-2,-1) # (B, nh, HW, hs)  # this head tends to all the pixels for these particular channels
+        v=v.view (B, self.n_head, head_size, H*W).transpose(-2,-1) # (B, nh, HW, hs)
 
-        y = y.transpose(1, 2).contiguous().view (B, C, H, W)
-        y = self.conv_proj (y)
+        #(HW HW) @ (HW, hs)
+        #att = k @ q * (1.0/math.sqrt(head_size))
+        #att = F.softmax (att, dim=-1) # (B, nh, HW, HW)
+        #y = att @ v.transpose (-1,-2) # (B, nh, HW, HW) @ (B, nh, HW, hs) -> (B, nh, HW, hs) same as B T T @ B T C
+        
+        y = F.scaled_dot_product_attention (q, k, v, is_causal=False) # returns aggregated info : batched as a specific head for entire pixel space for particular channels (B, nh, HW(or T), hs)
+        y = y.permute (0, 1, 3, 2).contiguous ().view (B, C, H, W)
+
+        y = self.conv_projection(y)
         return X + y
 
