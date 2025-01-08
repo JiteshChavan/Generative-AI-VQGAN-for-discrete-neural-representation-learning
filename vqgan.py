6a09e7822658a6aa7afe589025ed0636f52b6f46
Jitesh <jchavan010@gmail.com>
2025-01-08 15:24:59 -0500

    configure_optimizer function for vqgan model, play notebooks to inpsect tensor shapes for decays

--- vqgan.py
+++ vqgan.py
@@ -7,6 +7,8 @@ from quantizer import Quantizer
 from decoder import Decoder
 from dataclasses import dataclass
 
+import inspect
+
 @dataclass
 class VQGanConfig:
     pass
@@ -58,5 +60,50 @@ class VQGan (nn.Module):
         return 0.8 * lambda_factor
     
     # weight adopt function
+    # A function that turns off gan loss for first epoch
+    # as advised in VQGAN paper
+
+
+    # ---------------------------------------------------------------------------------------------------------------------------------
+    # optimizer configurations
+    # ---------------------------------------------------------------------------------------------------------------------------------
+    # weight decay:
+    # At initialization the weights are initialized so that all outcomes are equally likely,
+    # now we want the weights to be optimized (pushed or pulled) so that likely outcomes are assigned higher probabilities
+    # but at the same time, the we subject the weights to a force, regularizing force that kind of works against optimizing force
+    # we decay the weights while stepping in the direction of gradients
+    # sort of a force against the optimization that pulls down the weights and makes the network use more of the weights, distributing the task across multiple channels if you would,
+    # to achieve the activations, basically regularization
+    
+    def configure_optimizers (self, weight_decay, learning_rate, device, optimizer_config_name):
+        # start with all parameters that require gradients
+        param_dict = {pn:p for pn,p in self.named_parameters()}
+        param_dict = {pn:p for pn,p in param_dict.items() if p.requires_grad}
+
+        # create optim groups. Any parameter that is 2D will be weight decayed, otherwise no.
+        # i.e. all tensors in matmul + embeddings decay, all biases and layer norms don't
+        decay_params = [p for pn, p in param_dict.items() if p.dim() >= 2]
+        no_decay_params = [p for pn, p in param_dict.items() if p.dim() < 2]
+
+        optim_groups = [
+            {'params' : decay_params, 'weight_decay' : weight_decay},
+            {'params' : no_decay_params, 'weight_decay' : 0.0}
+        ]
+
+        num_decay_params = sum (p.numel () for p in decay_params)
+        num_no_decay_params = sum (p.numel () for p in no_decay_params)
+
+        print (f"optimizer_config:{optimizer_config_name}:\n num decayed parameter tensors:{len(decay_params)} with {num_decay_params} parameters")
+        print (f"num non-decayed parameter tensors:{len(no_decay_params)} with {num_no_decay_params} parameters")
+
+        # Create AdamW optimizer and use the fused version if it is available
+        fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+        use_fused = fused_available and 'cuda' in device # or device == 'cuda'
+        print (f"using fused AdamW:{use_fused}")
+        # kernel fusion for AdamW update instead of iterating over all the tensors to step which is a lot slower.
+        optimizer = torch.optim.AdamW (optim_groups, learning_rate, betas=(0.9,0.95), eps=1e-8, use_fused=use_fused)
+        return optimizer
+
+
 
 
